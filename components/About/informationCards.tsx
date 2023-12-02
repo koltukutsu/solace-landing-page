113d89ea1b9550e92b68844658bb894bf77fbc73
koltukustu <l6918040@std.yildiz.edu.tr>
2023-12-02 06:23:24 +0000

    Update: update the images

--- components/About/informationCards.tsx
+++ components/About/informationCards.tsx
@@ -2,24 +2,24 @@ import { InformationCard } from "@/types/informationCard";
 
 const informationCardsList: InformationCard[] = [
     {
-        title:"Birinci",
-        description:"Ikinci",
-        image:"https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_QL75_UX380_CR0,1,380,562_.jpg",
+        title:"Sürdürülebilirlik",
+        description:"Hayatı sürdür",
+        image:"https://media.discordapp.net/attachments/1011207439464546384/1180006478153785345/semihbbcn_a_tulip_germinating_good_lighting_high_quality_bffdefc4-64f4-4dbf-8ef9-c27be8fd090f.png?ex=657bd9ab&is=656964ab&hm=469edfa607f7b45ec66eceeb644eb5f6d93763b79e974029dd0536785496873d&=&format=webp&quality=lossless&width=1311&height=1311",
+    },
+    {
+        title:"Hayat",
+        description:"Hayat ateşi",
+        image:"https://media.discordapp.net/attachments/1011207439464546384/1180006465327595580/semihbbcn_a_tulip_germinating_good_lighting_high_quality_b540d2e4-1d0b-4599-ba3d-ab0643bc17dc.png?ex=657bd9a8&is=656964a8&hm=dc3d0679027af0acfb67cb8a1ad102e403afea1a015fab2f42908541076d861f&=&format=webp&quality=lossless&width=1311&height=1311",
     },
     {
         title:"Birinci",
         description:"Ikinci",
-        image:"https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_QL75_UX380_CR0,1,380,562_.jpg",
+        image:"https://media.discordapp.net/attachments/1011207439464546384/1180006465327595580/semihbbcn_a_tulip_germinating_good_lighting_high_quality_b540d2e4-1d0b-4599-ba3d-ab0643bc17dc.png?ex=657bd9a8&is=656964a8&hm=dc3d0679027af0acfb67cb8a1ad102e403afea1a015fab2f42908541076d861f&=&format=webp&quality=lossless&width=1311&height=1311",
     },
     {
         title:"Birinci",
         description:"Ikinci",
-        image:"https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_QL75_UX380_CR0,1,380,562_.jpg",
-    },
-    {
-        title:"Birinci",
-        description:"Ikinci",
-        image:"https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_QL75_UX380_CR0,1,380,562_.jpg",
+        image:"https://media.discordapp.net/attachments/1011207439464546384/1180006465327595580/semihbbcn_a_tulip_germinating_good_lighting_high_quality_b540d2e4-1d0b-4599-ba3d-ab0643bc17dc.png?ex=657bd9a8&is=656964a8&hm=dc3d0679027af0acfb67cb8a1ad102e403afea1a015fab2f42908541076d861f&=&format=webp&quality=lossless&width=1311&height=1311",
     }
 ]
 
